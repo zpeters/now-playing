4fcf292f67591ef11d38a72faa3b9e847e77e72a
Zach Peters <zpeters@gmail.com>
2020-01-11 11:39:51 -0600

    read from mpc

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const child_process = require('child_process')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+function getNowPlaying() {
+    var child = child_process.spawnSync("mpc", ["status"])
+    var stdout = child.stdout.toString();
+
+    console.log("stdout ", stdout)
+    var firstLine = stdout.split("\n")[0].trim();
+
+    return firstLine
+}
+
+app.get('/now-playing', (req, res) => {
+    res.send(getNowPlaying())
 })
 
 app.listen(port, () => console.log(`listening on port ${port}`))
+
